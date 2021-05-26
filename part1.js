f62c6ac7812d9964543e5a251c2c8154e401ca98
adam <paddenadam@gmail.com>
2021-05-26 17:02:27 -0400

    finished all required exercises

--- part1.js
+++ part1.js
@@ -29,3 +29,34 @@ db.people.find( { $and: [
     {age: {$lte: 45} },
     {age: {$gt: 40} },
     ]})
+
+// 8. List people whose first name starts with "H". (2)
+db.people.find({first_name: /^H/})
+
+// 9. List all people in Michigan, sorted by first name. (6)
+db.people.find({state: "Michigan"}).sort({first_name: 1})
+
+// 10. List all people who live in Virginia or are named Virginia.
+db.people.find({$or: [
+    {state: "Virginia"},
+    {first_name: "Virginia"}
+    ]})
+
+// 11. List the names of people under age 30. Only display their first and last name. (38)
+db.people.find({age: {$lt: 30}}, {first_name: 1, last_name: 1 })
+
+// 12. List all people in Montana. Display all information except age. (2)
+db.people.find({state: "Montana"}, {age: 0})
+
+// 13. List the email addresses of people with a ".edu" email. Only display the email. (12)
+db.people.find({email: /.edu$/}, {email: 1})
+
+// Extended Challenges
+// 14. Count all people with at least one child under age four. (69)
+db.people.find({ 'children.age': {$lt: 4}}).count()
+
+// 15. List people who have no children. (43)
+db.people.find({children: {$size: 0}})
+
+// 16. List people who have at least one child. (157)
+db.people.find({children: {$ne: [ ]}})
